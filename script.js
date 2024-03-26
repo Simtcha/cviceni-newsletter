c7b6ca71e4a8775f3f6ba364e992305e80906c28
Sim P <anomis.p@gmail.com>
2024-03-26 14:04:01 +0100

    code ready

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-
 /*
-const formular = document.querySelector("#registrace")
+V CSS si vytvořte třídu, která dělá výrazný červený rámeček kolem textového políčka.
+Přidejte na textové políčko posluchač události input.
+Během toho, jak uživatel píše, kontrolujte, jestli náhodou políčko není prázdné. 
+V takovém případě na políčko přidejte třídu s červeným rámečkem. Pokud je vstup v pořádku, rámeček odeberte.
+Zobrazte červený rámeček i v případě, že zadaný e-mail neobsahuje zavináč. K tomu můžete použít metodu indexOf nebo includes.
+*/
 
-formular.addEventListener("submit", (event) => {
+
+const formular = document.querySelector("#newsletter-reg")
+const emailik = document.querySelector("#email")
+
+
+const redFrame = (event) => {
+    if (event.target.value === "" || !event.target.value.includes('@')) {
+        emailik.classList.add("ramecek")
+    } else {
+        emailik.classList.remove("ramecek")
+    }
+} 
+
+const newMessage = (event) => {
     event.preventDefault()
-    const jmenoTxt = document.querySelector("#jmeno")
-    const prijmeniTxt = document.querySelector("#prijmeni")
-    alert(`Uzivatel ${jmenoTxt.value} ${prijmeniTxt.value} uspesne registrovan!`)
-})*/
-
-
-
-
-
-const elForm = document.querySelector("#newsletter-reg")
-const oznameni = document.querySelector("#news")
-
-elForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const emailEl = document.querySelector("#email")
-    oznameni.innerHTML = `<p>Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${emailEl.value}</p>`
-    
-})
-
-
-
-
-/*
-elHeslo.addEventListener("input", (event) => {
-8/
-
-
-elHeslo.addEventListener("change", (event) => {
-if(event.target.value.length < 8) {
-console.log(`Heslo je kratke, pripis ${8 - event.target.value.length}`)
-} else {
-console.log("Heslo je zabezpecene")
+    const oznameni = document.querySelector("#news")
+    const oznameni2 = document.querySelector("#news2")
+    oznameni.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${emailik.value}`
+    oznameni2.textContent = ``
 }
-})
+
+emailik.addEventListener("input", redFrame) 
+formular.addEventListener("submit", newMessage) 
+
+
+/*
+const redFrame = () => {
+    const emailikValue = emailik.value;
+    if (emailikValue === "" || !emailikValue.includes('@')) {
+        emailik.classList.add("ramecek");
+    } else {
+        emailik.classList.remove("ramecek");
+    }
+} */
+
+
